781bc6cefab0bb2f838090fb8b5d6df129bd0ffd
Cristian Delcid <falet5080@hotmail.com>
2020-08-15 18:40:31 -0600

    docs agregado

---
+++ docs/precache-manifest.7fc9d325a1491766592e767c93211645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b74ada1b4008cc956d56eeea8a5646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2275d6568c3afd9763b",
+    "url": "/static/css/main.fb1dc0ad.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2275d6568c3afd9763b",
+    "url": "/static/js/main.bc0ac361.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
